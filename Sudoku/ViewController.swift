bc874b520ffa4729ace81b0fa6e1f52c5152a427
Stephen Paul Brown <aerologic@ecs325-19.labs.encs>
2017-02-23 11:16:33 -0800

    Initial commit. Have the views contrained

---
+++ Sudoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SudokuLayout
+//
+//  Created by Stephen Paul Brown on 2/7/17.
+//  Copyright © 2017 Stephen Paul Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
