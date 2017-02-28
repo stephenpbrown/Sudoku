caecf2fce3df815025d438c3d932add1784625a4
Stephen Paul Brown <aerologic@ecs325-19.labs.encs>
2017-02-28 09:11:04 -0800

    Added glyphs for the buttons

--- Sudoku/AppDelegate.swift
+++ Sudoku/AppDelegate.swift
@@ -13,12 +13,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var sudoku : SudokuPuzzle?
+    v//ar puzzleView : PuzzleView?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         self.sudoku = SudokuPuzzle()
         self.sudoku!.loadPuzzle(puzzleString: "simple")
+        
+        //self.puzzleView = PuzzleView()
         
         return true
     }
