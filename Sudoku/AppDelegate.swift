6d38ad9c7bf2c4d546dd78cd32472041180508b0
Stephen Paul Brown <aerologic@ecs325-19.labs.encs>
2017-02-28 09:11:45 -0800

    Fixed a comment issue

--- Sudoku/AppDelegate.swift
+++ Sudoku/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var sudoku : SudokuPuzzle?
-    v//ar puzzleView : PuzzleView?
+    //var puzzleView : PuzzleView?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
